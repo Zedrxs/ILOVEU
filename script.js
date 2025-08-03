c94f4f62f815f7fd1417eeca5703bbc0e66f01ad
Zedrxs <zedrus.img@outlook.de>
2025-08-03 07:30:48 -0700

    WEB 1.1

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 (() => {
   const todayISO = new Date().toISOString().split('T')[0];
+  const bg = document.getElementById('background');
+  const heart = document.getElementById('heart');
+  const container = document.getElementById('buttonContainer');
+  const infoBox = document.getElementById('infoBox');
+  const textboxContent = document.getElementById('textboxContent');
 
   document.querySelectorAll('.glow-button').forEach(button => {
-    const btnDate = button.dataset.date; // Format YYYY-MM-DD
-
-    if (btnDate === todayISO) {
-      // Aktuelles Datum: Button ist klickbar
+    const btnDate = button.dataset.date;
+    const btnText = button.dataset.text;
+    if (btnDate <= todayISO) {
       button.addEventListener('click', () => {
-        const bg = document.getElementById('background');
-        const heart = document.getElementById('heart');
-        const container = document.getElementById('buttonContainer');
-
-        // Background rot
         bg.style.backgroundColor = 'darkred';
-
-        // Buttons verschwinden
         container.classList.add('fade-out');
-
-        // Herz transparent ausblenden
         heart.style.transition = 'opacity 1s ease-out';
         heart.style.opacity = '0';
+        infoBox.classList.remove('hidden');
+        textboxContent.innerText = btnText;
       });
     } else {
-      // Andere Buttons deaktivieren
       button.style.opacity = 0.5;
       button.style.cursor = 'not-allowed';
     }
   });
 })();
+
 
 button.addEventListener('click', () => {
   const bg = document.getElementById('background');
